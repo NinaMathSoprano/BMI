70ab9f1d4607878f4318a4aadbd54ab4189179f9
Nina Bukreeva <Ineya566@gmail.com>
2024-10-17 00:02:40 +0300

    игнор указанных папок

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class BmiService {
+public class BmiService {
+    public int calculate(float height, float weight) {
+
+        System.out.println("масса = " + weight);
+        System.out.println("рост = " + height);
+
+        float i = weight/(height*height);
+
+        return (int) i;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        BmiService service = new BmiService();
+        float a = 1.87F;
+        float b = 98;
+        int index = service.calculate(a, b);
+        System.out.println(index);
     }
 }
